33035e311136a88252825c40bda8c2b09e8be585
Mike Bleske <mike.bleske@fanatics.live>
2026-04-08 09:19:32 -0700

    Enable analytics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <>
+    <App />
+    <Analytics />
+  </>
+);
